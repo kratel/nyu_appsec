798148982efeb3f845790734a06c36149b936d67
kratel <santi.f.salas@gmail.com>
2019-09-15 15:42:52 -0400

    Init spell.c file

---
+++ spell.c
@@ -0,0 +1,24 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "dictionary.h"
+
+int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]){
+	//First Test
+	int i;
+	i = 1;
+	return i;
+}
+
+bool check_word(const char* word, hashmap_t hashtable[]){
+	//First Test
+	bool i;
+	i = false;
+	return i;
+}
+
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
+	//First Test
+	bool i;
+	i = false;
+	return i;
+}
