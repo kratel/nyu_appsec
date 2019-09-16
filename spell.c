7faf4a53ec2323efa38a9a2cba69afdbbcd7f7c8
kratel <santi.f.salas@gmail.com>
2019-09-15 23:40:37 -0400

    Initial version of check_word. Fized word termination error in check_words, fixed malloc error in load_dictionary. Something going wrong with strcmp.

--- spell.c
+++ spell.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "dictionary.h"
 
 int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]){
 	int num_misspelled = 0;
 	int counter = 0;
 	int c;
-	char w[LENGTH];
+	char w[LENGTH + 1];
 	char* word;
 	word = w;
 	while ((c = fgetc(fp)) != EOF){
@@ -21,14 +22,14 @@ int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]){
 				c = fgetc(fp);
 				if (c == EOF){
 					word[counter] = '\0';
-					misspelled[num_misspelled] = (char *) malloc(counter * sizeof(char));
+					misspelled[num_misspelled] = (char *) malloc((counter + 1) * sizeof(char));
 					strcpy(misspelled[num_misspelled],word);
 					num_misspelled++;
 					return num_misspelled;
 				}
 			}
 			word[counter] = '\0';
-			misspelled[num_misspelled] = (char *) malloc(counter * sizeof(char));
+			misspelled[num_misspelled] = (char *) malloc((counter + 1) * sizeof(char));
 			strcpy(misspelled[num_misspelled],word);
 			num_misspelled++;
 			word[0] = '\0';
@@ -41,10 +42,12 @@ int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]){
 				if (!check_word(word, hashtable)){
 					// add to misspelled array
 					// need to malloc memory
-					misspelled[num_misspelled] = (char *) malloc(LENGTH * sizeof(char));
+					misspelled[num_misspelled] = (char *) malloc((LENGTH + 1) * sizeof(char));
 					strcpy(misspelled[num_misspelled],word);
 					num_misspelled++;
 				}
+				word[0] = '\0';
+				counter = 0;
 			} else {
 				/* add to word */
 				word[counter] = c;
@@ -52,15 +55,54 @@ int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]){
 			}
 		}
 	}
-	printf("test\n");
 	return num_misspelled;
 }
 
 bool check_word(const char* word, hashmap_t hashtable[]){
-	//First Test
-	bool i;
-	i = false;
-	return i;
+	// Strip punctuation at beginning of word
+	char modword[LENGTH];
+	char *nb = modword;
+	// must use b as new word start.
+	const char *b = word;
+	int pos_to_first = 0;
+	while (ispunct(*b)){
+		pos_to_first++;
+		b++;
+	}
+	strcpy(nb, b);
+	// Strip puncation at end of word
+	//  pointer to last char in word
+	const char *e = word + strlen(word) - 1;
+	int pos_to_last = 0;
+	//  keep going backwards setting pointer to 0 as punct is found
+	//. make sure you don't go past the new beggining of word.
+	while (ispunct(*e) && b < e) {
+		pos_to_last++;
+		e--;
+	}
+	modword[strlen(word) - (pos_to_last + pos_to_first)] = '\0';
+	printf("Word converted from: %s to %s\n", word, nb);
+	int bucket;
+	bucket = hash_function(nb);
+	hashmap_t cursor = hashtable[bucket];
+	while(cursor){
+		// check if word is at cursor
+		printf("checking if word matches cursor: %s\n", cursor->word);
+		if (strcmp(cursor->word, nb) == 0){
+			printf("\tMatched original\n");
+			return true;
+		} else {
+			for(int i = 0; i < strlen(nb) - 1; i++){
+				nb[i] = tolower(nb[i]);
+			}
+			if( strcmp(nb, cursor->word) == 0){
+				printf("\tMatched lowercase\n");
+				return true;
+			}
+		}
+		cursor = cursor->next;
+	}
+	return false;
 }
 
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
@@ -102,7 +144,7 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
 			/* check if c is \r \n \t or whitespace which means end of word */
 			if (c == '\r' || c == '\n' || c == '\t' || c == ' '){
 				/* add word in */
-				hashmap_t new_node = (hashmap_t) malloc(LENGTH * sizeof(char));
+				hashmap_t new_node = (hashmap_t) malloc(sizeof(node));
 				new_node->next = NULL;
 				word[counter] = '\0';
 				strcpy(new_node->word,word);
